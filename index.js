f64895edf03363c739ffd38dafba0847ba649ace
Michael van Dalen <michaelvdalen191@gmail.com>
2024-11-24 11:55:20 +0200

    Added final styling changes and last bit of JS

--- index.js
+++ index.js
@@ -28,12 +28,20 @@ addButtonEl.addEventListener("click", function () {
 });
 
 onValue(shoppingListInDB, function (snapshot) {
-  let itemsArray = Object.values(snapshot.val());
+  if (snapshot.exists()) {
+    let itemsArray = Object.entries(snapshot.val());
 
-  clearShoppingListEl();
+    clearShoppingListEl();
 
-  for (let i = 0; i < itemsArray.length; i++) {
-    appendItemToShoppingListEl(itemsArray[i]);
+    for (let i = 0; i < itemsArray.length; i++) {
+      let currentItem = itemsArray[i];
+
+      let currenItemID = currentItem[0];
+      let currentItemValue = currentItem[1];
+      appendItemToShoppingListEl(currentItem);
+    }
+  } else {
+    shoppingListEl.innerHTML = "No items here... yet";
   }
 });
 
@@ -45,6 +53,22 @@ function clearInputFieldEl() {
   inputFieldEl.value = "";
 }
 
-function appendItemToShoppingListEl(itemValue) {
-  shoppingListEl.innerHTML += `<li>${itemValue}</li>`;
+function appendItemToShoppingListEl(item) {
+  let itemID = item[0];
+  let itemValue = item[1];
+
+  let newEl = document.createElement("li");
+
+  newEl.textContent = itemValue;
+
+  newEl.className =
+    "p-4 bg-[#ffa500] hover:bg-[#FFECC7] shadow-xl rounded-md grow text-center cursor-pointer";
+
+  newEl.addEventListener("click", function () {
+    let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`);
+
+    this.remove(exactLocationOfItemInDB);
+  });
+
+  shoppingListEl.appendChild(newEl);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        "8xl": "380px",
+      },
+      boxShadow: {
+        customShadow: "0px 1px 4px rgba(0, 0, 0, 0.2)",
+      },
+    },
   },
   plugins: [],
 };
